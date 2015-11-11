7e8c0efaacee391c953205ed246b1f7466e2e1cd
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-11 09:51:53 +0000

    init

---
+++ EDC-Trabalho4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EDC_Trabalho4.Startup))]
+namespace EDC_Trabalho4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
