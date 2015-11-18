6da78b3de365282aae0935839604e5a4d84513df
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-18 09:18:18 +0000

    added

---
+++ EDC-Trabalho4/feed.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="feed.aspx.cs" Inherits="EDC_Trabalho4.feed" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+   
+
+    
+    
+
+   
+
+    
+    <asp:FormView ID="FormView1" runat="server">
+    </asp:FormView>
+
+   
+
+    
+    
+
+   
+
+    
+    <asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/App_Data/blitz_feed.rss" TransformFile="~/App_Data/channel.xsl"></asp:XmlDataSource>
+
+    
+</asp:Content>
---
+++ EDC-Trabalho4/feed.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace EDC_Trabalho4
+{
+    public partial class feed : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string url = "http://feeds.feedburner.com/PublicoRSS?format=xml";
+                XmlReader reader = XmlReader.Create(url);
+                XmlDocument doc = new XmlDocument();
+                doc.Load(reader);
+                reader.Close();
+
+                //XmlDataSource1.Data = "~/App_data/blitz_fedd.rss";
+                //XmlDataSource1.XPath = "rss/channel";
+                //FormView1.DataSource = XmlDataSource1;
+                //FormView1.DataBind();
+            }
+        }
+    }
+}
